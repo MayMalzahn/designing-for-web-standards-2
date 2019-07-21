d606941e0334d9ae87130205297c5716e4ef256f
MayjentaMalzahn <42015266+MayMalzahn@users.noreply.github.com>
2019-07-20 20:19:55 -0400

    Added validation proof

--- HikersGuide/js/content.js
+++ HikersGuide/js/content.js
@@ -10,22 +10,44 @@ xhr.onload = function()
 	var footerSelect = document.querySelector('#foot');
 	if(footerSelect)
 	{
+		footerSelect.querySelectorAll('h4')[2].innerHTML = data.about.title;
+		footerSelect.querySelector('p').innerHTML = data.about.text;
 		
+		for(var i = 0; i < 4; i++)
+		{
+			footerSelect.querySelectorAll('#eventList li')[i].innerHTML = data.events[i].title;
+		}
+		for(var a = 0; a < 4; a++)
+		{
+			footerSelect.querySelectorAll('#hikerList li')[a].innerHTML = data.hikers[a].firstname + " " + data.hikers[a].lastname;
+		}
 	}
 	
 	var blogSelect = document.querySelector('#blog');
 	
 	if(blogSelect)
 	{
-		
+		for(var b = 0; b < 3; b++)
+		{
+			blogSelect.querySelectorAll('h2')[b].innerHTML = data.posts[b].title;
+			blogSelect.querySelectorAll('h3')[b].innerHTML = data.posts[b].postDate;
+			blogSelect.querySelectorAll('p')[b].innerHTML = data.posts[b].text;
+		}
 	}
 	var eventSelect = document.querySelector('#events');
 	
 	if(eventSelect)
 	{
-		
+		for(var c = 0; c < 3; c++)
+		{
+			
+			eventSelect.querySelectorAll('h2')[c].innerHTML = data.events[c].title;
+			eventSelect.querySelectorAll('h3')[c].innerHTML = data.events[c].date;
+			eventSelect.querySelectorAll('p')[c].innerHTML = data.events[c].text;
+			
+		}
 	}
 	
-}
+};
 xhr.open('GET','https://joshbloom.github.io/dws1/data/hikersguide.json', true);
 xhr.send(null);
